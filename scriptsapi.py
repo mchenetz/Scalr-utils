869fdaaaa51c6ab77d22ce76bd08e19590645b32
Michael Chenetz <mike@chenetz.net>
2017-04-17 10:40:00 -0400

    scriptsapi.py: Created class and set environment as initial value.

--- scriptsapi.py
+++ scriptsapi.py
@@ -1,50 +1,65 @@
 from api.client import ScalrApiClient
 import os
 
+## Set Scalr keys and URL. I have them set in the environment for development purposes.
 SCALR_API_KEY = os.environ['SCALR_API_KEY']
 SCALR_SECRET_KEY = os.environ['SCALR_SECRET_KEY']
 url = os.environ['SCALR_URL']
 
+## Define Environments so you do not have to remember ids.
+env = {
+    'dc-lab':1
+}
+
 client = ScalrApiClient(url.rstrip("/"),SCALR_API_KEY,SCALR_SECRET_KEY)
 
-def getIdFromName(envId, name):
-    scripts = client.fetch('/api/v1beta0/user/{envId}/scripts/'.format(envId=envId))
-    for script in scripts:
-        if(script['name'] == name):
-            return (script['id'])
-    else:
-        return 0
+class scriptsapi(object):
 
-def getScript(envId, scriptId):
-    return client.fetch('/api/v1beta0/user/{envId}/scripts/{scriptid}'.format(envId=envId, scriptid=scriptId))
+    def __init__(self, env):
+        self.env = env
 
-def listScripts(envId):
-    return client.fetch('/api/v1beta0/user/{envId}/scripts/'.format(envId=envId))
+    def getIdFromName(self, name):
+        scripts = client.fetch('/api/v1beta0/user/{envId}/scripts/'.format(envId=self.env))
+        for script in scripts:
+            if(script['name'] == name):
+                return (script['id'])
+        else:
+            return 0
 
-def listScriptVersions(envId, scriptId):
-    return client.fetch('/api/v1beta0/user/{envId}/scripts/{scriptId}/script-versions/'.format(envId=envId, scriptId=scriptId))
+    def getScript(self, scriptId):
+        return client.fetch('/api/v1beta0/user/{envId}/scripts/{scriptid}'.format(envId=self.env, scriptid=scriptId))
 
-def writeScriptVersionToFile(envId, scriptId, directory):
-    versions = client.fetch('/api/v1beta0/user/{envId}/scripts/{scriptId}/script-versions/'.format(envId=envId, scriptId=scriptId))
-    for version in versions:
-        path = os.path.abspath(directory) + '\\' + getScript(scriptId)['name'] + str(version['version'])
-        with open(path, 'w') as write:
-            write.writelines(version['body'])
-            write.close()
+    def listScripts(self):
+        return client.fetch('/api/v1beta0/user/{envId}/scripts/'.format(envId=self.env))
+
+    def listScriptVersions(self, scriptId):
+        return client.fetch('/api/v1beta0/user/{envId}/scripts/{scriptId}/script-versions/'.format(envId=self.env, scriptId=scriptId))
+
+    def writeScriptVersionToFile(self, scriptId, directory):
+        versions = client.fetch('/api/v1beta0/user/{envId}/scripts/{scriptId}/script-versions/'.format(envId=self.env, scriptId=scriptId))
+        for version in versions:
+            path = os.path.abspath(directory) + '\\' + getScript(scriptId)['name'] + str(version['version'])
+            with open(path, 'w') as write:
+                write.writelines(version['body'])
+                write.close()
 
 
-for list in listScripts(1):
+## init API with environment
+api = scriptsapi(env['dc-lab'])
+
+## Print a listing of all Scripts in an environment
+for list in api.listScripts():
     print list['name']
 
 ## Get Script by name and display the script
-## getScript(Environment, ScriptID)
+## getScript(ScriptID)
 ## ** In This case i used getIDFromName to input the ID from a given name **
-print (getScript(1, getIdFromName(1,'rundeck-install')))
+print (api.getScript(api.getIdFromName('rundeck-install')))
 
 ## List Versions of the Script
-## listScriptVersions(environment, ScriptID)
-print (listScriptVersions(1, 1)[0])
+## listScriptVersions(ScriptID)
+print (api.listScriptVersions(api.getIdFromName('theforeman-installer'))[0])
 
 ## Write All Script versions of a particular Script to a directory
 ## writeScriptVersionToFile(Environment, ScriptID, Directory)
-## writeScriptVersionToFile(1,1,'C://Users//user//Documents//sscripts//')
+## writeScriptVersionToFile(env['dc-lab'],getIdFromName(env['dc-lab'],'theforeman-installer'),'C://Users//user//Documents//sscripts//')
