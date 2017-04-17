f3cf20f7c76977a2c2de039aada6dc29f4b46c46
Michael Chenetz <mike@chenetz.net>
2017-04-17 11:38:57 -0400

    Seperated Examples into example.py

---
+++ example.py
@@ -0,0 +1,31 @@
+from scriptsapi import scriptsapi
+import os
+
+## EXAMPLES
+## Define Environments so you do not have to remember ids.
+env = {
+    'dc-lab':1
+}
+SCALR_API_KEY = os.environ['SCALR_API_KEY']
+SCALR_SECRET_KEY = os.environ['SCALR_SECRET_KEY']
+url = os.environ['SCALR_URL']
+
+## init API with environment
+api = scriptsapi(env['dc-lab'],url,SCALR_API_KEY,SCALR_SECRET_KEY)
+
+## Print a listing of all Scripts in an environment
+for list in api.listScripts():
+    print list['name']
+
+## Get Script by name and display the script
+## getScript(ScriptID)
+## ** In This case i used getIDFromName to input the ID from a given name **
+print (api.getScript(api.getIdFromName('rundeck-install')))
+
+## List Versions of the Script
+## listScriptVersions(ScriptID)
+print (api.listScriptVersions(api.getIdFromName('theforeman-installer'))[0])
+
+## Write All Script versions of a particular Script to a directory
+## writeScriptVersionToFile(Environment, ScriptID, Directory)
+## api.writeScriptVersionsToFile(env['dc-lab'],api.getIdFromName('theforeman-installer'),'C://Users//user//Documents//sscripts//')
--- scriptsapi.py
+++ scriptsapi.py
@@ -31,36 +31,8 @@ class scriptsapi(ScalrApiClient):
     def writeScriptVersionsToFile(self, scriptId, directory):
         versions = self.fetch('/api/v1beta0/user/{envId}/scripts/{scriptId}/script-versions/'.format(envId=self.env, scriptId=scriptId))
         for version in versions:
-            path = os.path.abspath(directory) + '\\' + api.getScript(scriptId)['name'] + str(version['version'])
+            path = os.path.abspath(directory) + '\\' + self.getScript(scriptId)['name'] + str(version['version'])
             with open(path, 'w') as write:
                 write.writelines(version['body'])
                 write.close()
 
-## EXAMPLES
-## Define Environments so you do not have to remember ids.
-env = {
-    'dc-lab':1
-}
-SCALR_API_KEY = os.environ['SCALR_API_KEY']
-SCALR_SECRET_KEY = os.environ['SCALR_SECRET_KEY']
-url = os.environ['SCALR_URL']
-
-## init API with environment
-api = scriptsapi(env['dc-lab'],url,SCALR_API_KEY,SCALR_SECRET_KEY)
-
-## Print a listing of all Scripts in an environment
-for list in api.listScripts():
-    print list['name']
-
-## Get Script by name and display the script
-## getScript(ScriptID)
-## ** In This case i used getIDFromName to input the ID from a given name **
-print (api.getScript(api.getIdFromName('rundeck-install')))
-
-## List Versions of the Script
-## listScriptVersions(ScriptID)
-print (api.listScriptVersions(api.getIdFromName('theforeman-installer'))[0])
-
-## Write All Script versions of a particular Script to a directory
-## writeScriptVersionToFile(Environment, ScriptID, Directory)
-## api.writeScriptVersionsToFile(env['dc-lab'],api.getIdFromName('theforeman-installer'),'C://Users//user//Documents//sscripts//')
